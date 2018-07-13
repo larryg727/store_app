ce22aab815f0c612d5ac6330d8d0034b4a577fb4
larryg727 <larrygonzales727@gmail.com>
2018-07-13 01:14:25 -0500

    Fixed cors configuration to only accept request from frontend app.

--- store/__init__.py
+++ store/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000"]}})
 app.config.from_object(Config)
 
 from store import routes
--- store/routes.py
+++ store/routes.py
@@ -5,21 +5,23 @@ from store import utils
 from store import data_service
 
 
-@app.route('/product/create', methods=['POST'])
+@app.route('/api/add/product', methods=['POST'])
 def create_product():
     '''
     Creates product and saves to DB
     '''
+
     product_id = data_service.save_new_product(request.form)
     print(product_id)
     return 'true', 200
 
 
-@app.route('/products')
+@app.route('/api/products')
 def get_products():
     '''
     Returns list of products
     '''
+    print(request.headers)
     products = data_service.get_all_products()
     final_products = utils.prepare_product_data_for_return(products)
 
